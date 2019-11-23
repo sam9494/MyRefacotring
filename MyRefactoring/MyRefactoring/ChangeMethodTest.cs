c9f4963ba8d1f42000a682d5c004dd5328edf391
sam9494 <samchong9494@gmail.com>
2019-11-23 21:34:12 +0800

    add new method and old method situation

--- MyRefactoring/MyRefactoring/ChangeMethodTest.cs
+++ MyRefactoring/MyRefactoring/ChangeMethodTest.cs
@@ -6,14 +6,21 @@ namespace MyRefactoring
     public class ChangeMethodTest
     {
         [Test]
-        public void should_call_new_method()
+        public void should_call_old_method()
         {
             var changeMethod = new ChangeMethod();
             var expert= "This is Old Method";
             var actual= changeMethod.OldMethod();
-
             Assert.AreEqual(expert,actual);
+        }
 
+        [Test]
+        public void should_call_new_method()
+        {
+            var changeMethod = new ChangeMethod();
+            var expert= "This is New Method";
+            var actual = changeMethod.NewMethod();
+            Assert.AreEqual(expert,actual);
         }
     }
 }
