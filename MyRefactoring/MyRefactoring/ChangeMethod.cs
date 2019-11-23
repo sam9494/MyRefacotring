61b06930cb08ecf9f88bb3296ff966a1258d4232
sam9494 <samchong9494@gmail.com>
2019-11-23 21:27:10 +0800

    init project

---
+++ MyRefactoring/MyRefactoring/ChangeMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyRefactoring
+{
+    public interface IChangeMethod
+    {
+        string OldMethod();
+        string NewMethod();
+    }
+
+    public class ChangeMethod : IChangeMethod
+    {
+        public string OldMethod()
+        {
+            return "This is Old Method";
+        }
+
+        public string NewMethod()
+        {
+            return "This is New Method";
+        }
+    }
+}
---
+++ MyRefactoring/MyRefactoring/ChangeMethodTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MyRefactoring
+{
+    [TestFixture]
+    public class ChangeMethodTest
+    {
+        [Test]
+        public void should_call_new_method()
+        {
+            var changeMethod = new ChangeMethod();
+            var expert= "This is Old Method";
+            var actual= changeMethod.OldMethod();
+
+            Assert.AreEqual(expert,actual);
+
+        }
+    }
+}
